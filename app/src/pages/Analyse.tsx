aa7eef2ed6df9c55d1bb25ce52bb76aa67cfbe4d
Lucas Q <lucas.queveau44190@gmail.com>
2025-11-24 14:11:45 +0100

    feat test some thing with api

--- app/src/pages/Analyse.tsx
+++ app/src/pages/Analyse.tsx
@@ -1,9 +1,61 @@
+import { useEffect, useState } from "react";
 import { Header } from "../components/Header";
 
+
+// CECI EST UN TEST NE SURTOUT PAS PULL REQUEST CAR CE N'EST PAS CE QUI EST DEMANDER
+
+
+interface Filming {
+  id_lieu: string;
+  annee_tournage: string;
+  type_tournage: string;
+  nom_tournage: string;
+  nom_realisateur: string;
+  nom_producteur: string;
+  adresse_lieu: string;
+  ardt_lieu: string;
+  date_debut: string;
+  date_fin: string;
+  coord_x: number;
+  coord_y: number;
+}
+
+interface ApiResponse {
+  total_count: number;
+  results: Filming[];
+}
+
+async function testApi(): Promise<ApiResponse> {
+  const res = await fetch(
+    "https://opendata.paris.fr/api/explore/v2.1/catalog/datasets/lieux-de-tournage-a-paris/records?limit=3"
+  );
+
+  return res.json() as Promise<ApiResponse>;
+}
+
 export default function Analyse() {
+
+  const [data, setData] = useState<ApiResponse | null>(null)
+
+  useEffect(() => {
+    testApi().then((res) => setData(res))
+  }, [])
+
   return (
     <div>
       <Header />
+
+      <p>
+        {data ? (
+          <ul>
+            {data.results.map((film) => (
+              <li key={film.id_lieu}>{film.nom_tournage}</li>
+            ))}
+          </ul>
+        ) : (
+          <p>Chargement...</p>
+        )}
+      </p>
     </div>
   );
 }
