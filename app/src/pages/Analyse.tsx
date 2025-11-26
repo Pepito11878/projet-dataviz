df8ba632ab3cca4e8fb1fa6661b409944680f342
Lucas Q <lucas.queveau44190@gmail.com>
2025-11-26 11:35:43 +0100

    Update Analyse.tsx

--- app/src/pages/Analyse.tsx
+++ app/src/pages/Analyse.tsx
@@ -1,15 +1,15 @@
 import FirstGraph from "../components/FirstGraph";
 import { Header } from "../components/Header";
-import ThirdGraph from "../components/ThirdGraph";
 import SecondGraph from "../components/SecondGraph";
+import ThirdGraph from "../components/ThirdGraph";
 
 export default function Analyse() {
   return (
     <div>
       <Header />
       <FirstGraph />
-      <ThirdGraph />
       <SecondGraph />
+      <ThirdGraph />
     </div>
   );
 }
